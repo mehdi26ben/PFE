05c0456a93fd349a1b9315873f58a0affcef73a6
mehdi <mehdi.bentoufile@hotmail.fr>
2023-04-04 23:02:44 +0000

    4/4/2023 11:02 PM

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION["admin"])){
-        header("location:admin.php");
+        header("location:admin_login.php");
     }
 ?>
 <!DOCTYPE html>
@@ -25,12 +25,12 @@
         <div class="row flex-nowrap">
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <a href="admin.php" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <span class="fs-5 d-none d-sm-inline">Menu</span>
                     </a>
                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                         <li class="nav-item">
-                            <a href="#" class="nav-link align-middle px-0">
+                            <a href="admin.php" class="nav-link align-middle px-0">
                                 <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
                             </a>
                         </li>
@@ -54,7 +54,7 @@
                                 </li>
                             </ul>
                         </li>
-                        <li>
+                        <!--<li>
                             <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                                 <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
                             <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
@@ -71,16 +71,16 @@
                                     <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
                                 </li>
                             </ul>
-                        </li>
+                        </li>-->
                         <li>
-                            <a href="#" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                            <a href="admin_logout.php" class="nav-link px-0 align-middle">
+                            <i class="fa-solid fa-right-from-bracket"></i> <span class="ms-1 d-none d-sm-inline">logout</span> </a>
                         </li>
                     </ul>
                     <hr>
                     <div class="dropdown pb-4" id="dropdown">
                         <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                        <i class="fa-solid fa-bars"></i>
                             <span class="d-none d-sm-inline mx-1">loser</span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
--- admin_products.php
+++ admin_products.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION["admin"])) {
+    header("location:admin_login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
